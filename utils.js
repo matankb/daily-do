7e4d5a58bf8e24879acb7e5257d297be8a62343a
Matan Kotler-Berkowitz <205matan@gmail.com>
2015-09-12 08:55:47 -0400

    Add elements to utils
    Added thing and thingwrap to utils object

--- utils.js
+++ utils.js
@@ -1,11 +1,13 @@
 (function() {
     window.utils = {
+        thing: document.getElementById("thing"),
+        thingWrap: document.getElementById("thing-wrap"),
         getDate: function() {
             var d = new Date();
             return new Date(d.getTime() - d.getTimezoneOffset() * 60000).toJSON().slice(0, 10) 
         },
-        getThing: function() {
-            return document.getElementById("thing");
-        }
+        getThingValue: function() {
+            return thing.value;
+        },
     }
 })()
