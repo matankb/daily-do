b20e67339f32f7cfbc3e0cda7ce3755a19e4f96d
nmraz <noamraz8@gmail.com>
2015-09-12 19:20:31 +0300

    clean up utils.js

--- utils.js
+++ utils.js
@@ -4,16 +4,15 @@
         thingWrap: document.getElementById("thing-wrap"),
         getDate: function() {
             var d = new Date();
-            return new Date(d.getTime() - d.getTimezoneOffset() * 60000).toJSON().slice(0, 10) 
+            d.setTime(d.getTime() - d.getTimezoneOffset() * 60000);
+            return d.toJSON().slice(0, 10);
         },
         getThingValue: function() {
             return this.thing.value;
         },
         setThingValue: function() {
             chrome.storage.sync.set({
-                thing: (function() {
-                   return utils.getThingValue(); 
-                })()
+                thing: this.getThingValue()
             })
         }
     }
