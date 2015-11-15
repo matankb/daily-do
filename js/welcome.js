0d52e89d20d9613f7252fc9bcafc11c6f788c3db
Matan Kotler-Berkowitz <205matan@gmail.com>
2015-11-15 17:03:02 -0500

    Add basic functionality to welcome.js

---
+++ js/welcome.js
@@ -0,0 +1,10 @@
+(function() {
+  chrome.storage.sync.get("playedTutorial", function(playedTutorial) {
+    if(!playedTutorial.playedTutorial) {
+      document.getElementById("loading").style.display = "none";
+      document.getElementById("oninstall").style.display = "block";
+      utils.oninstall.setTutorialPlayed();
+    }
+
+  });
+})();
