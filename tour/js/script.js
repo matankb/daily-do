39be5efe238d9f02172de94f5b2341d4f6c268b1
Matan Kotler-Berkowitz <205matan@gmail.com>
2015-11-21 15:40:13 -0500

    Make framework for running JS between steps

--- tour/js/script.js
+++ tour/js/script.js
@@ -1,5 +1,15 @@
 introJs()
   .start()
+  .onchange(function(targetElement)  {
+    switch(this._currentStep) {
+      case 1:
+        break;
+      case 2:
+        break;
+      default:
+        break;
+    }
+  })
   .onexit(function() {
     window.location = "../../newtab/newtab.html";
   })
