73c5c7e230f7fb943715f6abb052a010c2912a50
Matan Kotler-Berkowitz <205matan@gmail.com>
2015-12-01 16:54:20 -0500

    Remove unnecessary js

--- pages/welcome/js/script.js
+++ pages/welcome/js/script.js
@@ -1,12 +1,10 @@
 (function() {
   chrome.storage.sync.get("playedTutorial", function(playedTutorial) {
     if(!playedTutorial.playedTutorial) {
-      console.log(playedTutorial.playedTutorial)
-      document.getElementById("loading").style.display = "none";
       document.getElementById("oninstall").style.display = "block";
       utils.onInstall.setTutorialPlayed();
-    }else {
-
+    } else {
+      document.getElementById("onupdate").style.display = "block";
     }
   });
 })();
