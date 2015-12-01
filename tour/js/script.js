2bd6072069662acd09328f49ff1ef756654ba140
Matan Kotler-Berkowitz <205matan@gmail.com>
2015-12-01 15:18:12 -0500

    Add functionality to step number three

--- tour/js/script.js
+++ tour/js/script.js
@@ -3,7 +3,11 @@
   .start()
   .onchange(function(targetElement)  {
     switch(this._currentStep) {
-      case 1:
+      case 3:
+        //show edit controls
+        utils.editControls.wrap.style.opacity = 1.0;
+        //and put text in thing if empty
+        if(!utils.thing.value) utils.thing.value = "Go to the Gym";
         break;
       case 2:
         break;
