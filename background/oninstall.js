932946442801c4433d9057766178795861725814
Matan Kotler-Berkowitz <205matan@gmail.com>
2017-02-13 16:34:45 -0500

    Differentiate between updates and installation

--- background/oninstall.js
+++ background/oninstall.js
@@ -1,5 +1,11 @@
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.tabs.create({
-    "url": "../pages/welcome/welcome.html"
-  })
-})
+function onInstalled(e) {
+  if (e.reason === 'install') {
+    chrome.tabs.create({
+      "url": `../pages/welcome/welcome.html?reason=install`
+    })
+  } else if (e.reason === 'updated') {
+    chrome.storage.local.set({ updated: true });
+  }
+}
+
+chrome.runtime.onInstalled.addListener(onInstalled)
