1a9e7e094073e28ce343dead665815b6406445c2
Matan Kotler-Berkowitz <205matan@gmail.com>
2017-02-13 17:07:25 -0500

    Set default settings on install

--- background/oninstall.js
+++ background/oninstall.js
@@ -1,5 +1,19 @@
+// TODO: conditional defaults
+
+const defaults = {
+  thing: '',
+  date: '2000-01-01',
+  done: false,
+  updated: false,
+  options: {
+    backgroundColor: '#c5cefa',
+    doneButtonColor: '#009387'
+  }
+}
+
 function onInstalled(e) {
   if (e.reason === 'install') {
+    chrome.storage.sync.set(defaults);
     chrome.tabs.create({
       "url": `../pages/welcome/welcome.html?reason=install`
     })
