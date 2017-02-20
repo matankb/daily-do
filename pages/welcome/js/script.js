0010c904ad8b86cba34e571384478f05678379ec
Matan Kotler-Berkowitz <205matan@gmail.com>
2017-02-20 16:56:42 -0500

    Re-check for background color

--- pages/welcome/js/script.js
+++ pages/welcome/js/script.js
@@ -1,5 +1,12 @@
 (function() {
+  function setBackgound() {
     storage.get('options').then(opts => {
+      if (!opts.backgroundColor) {
+        // opts.backgroundColor sometimes takes a few seconds to be set by welcome script
+        return setBackgound();
+      }
       document.body.style.backgroundColor = opts.backgroundColor;
     })
+  }
+  setBackgound();
 })();
